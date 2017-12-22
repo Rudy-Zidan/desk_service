c17467bf6ad3664e688487ec28be759182d6843e
Abdulla Mahmoud <aboodchei@gmail.com>
2017-12-22 02:25:02 +0200

    refinements

--- lib/lynks_service_desk/formatter.rb
+++ lib/lynks_service_desk/formatter.rb
@@ -1,5 +1,29 @@
 module LynksServiceDesk
-  class Formatter
+  module Formatter
+
+    refine String do
+      def dehumanize
+        parameterize.gsub("-", "_")
+      end
+
+      def syminize
+        dehumanize.to_sym
+      end
+    end
+
+    refine Symbol do
+      [:humanize, :titleize].each do |method_name|
+        define_method method_name do
+          to_s.send(method_name)
+        end
+      end
+    end
+
+    using LynksServiceDesk::Formatter
+
+    def self.initial_state
+      byebug
+    end
 
   end
 end
