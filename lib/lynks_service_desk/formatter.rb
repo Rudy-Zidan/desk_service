c71b2dcb7aa1ec3333d329d3d12c48ccc59d5ef8
Abdulla Mahmoud <aboodchei@gmail.com>
2017-12-22 02:58:42 +0200

    add method_missing

--- lib/lynks_service_desk/formatter.rb
+++ lib/lynks_service_desk/formatter.rb
@@ -1,11 +1,27 @@
 module LynksServiceDesk
-  module Formatter
+    module Formatter
 
     using LynksServiceDesk::Refinements
 
-    def self.initial_state
-      byebug
+    def self.initial_state_symbol
+      initial_state.syminize
     end
+
+
+
+
+
+
+
+
+    def self.method_missing(meth, *args, &block)
+      if parent.configuration.respond_to? meth
+        parent.configuration.send(meth)
+      else
+        super
+      end
+    end
+
 
   end
 end
