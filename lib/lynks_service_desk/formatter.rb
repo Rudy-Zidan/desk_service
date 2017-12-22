20a391057ec96de8dd9596e8af3f4dffa23620fa
Abdulla Mahmoud <aboodchei@gmail.com>
2017-12-22 02:27:27 +0200

    Extract refinements

--- lib/lynks_service_desk/formatter.rb
+++ lib/lynks_service_desk/formatter.rb
@@ -1,25 +1,7 @@
 module LynksServiceDesk
   module Formatter
 
-    refine String do
-      def dehumanize
-        parameterize.gsub("-", "_")
-      end
-
-      def syminize
-        dehumanize.to_sym
-      end
-    end
-
-    refine Symbol do
-      [:humanize, :titleize].each do |method_name|
-        define_method method_name do
-          to_s.send(method_name)
-        end
-      end
-    end
-
-    using LynksServiceDesk::Formatter
+    using LynksServiceDesk::Formatter::Refinements
 
     def self.initial_state
       byebug
---
+++ lib/lynks_service_desk/formatter/refinements.rb
@@ -0,0 +1,30 @@
+module LynksServiceDesk
+  module Formatter
+    module Refinements
+      refine String do
+        def dehumanize
+          parameterize.gsub("-", "_")
+        end
+
+        def syminize
+          dehumanize.to_sym
+        end
+      end
+
+      refine Symbol do
+        [:humanize, :titleize].each do |method_name|
+          define_method method_name do
+            to_s.send(method_name)
+          end
+        end
+      end
+
+
+
+
+
+      using LynksServiceDesk::Formatter::Refinements
+      
+    end
+  end
+ end
