63f803818a67040b4927a570b5f09028f0a16002
Abdulla Mahmoud <aboodchei@gmail.com>
2017-12-24 04:52:56 +0200

    refactor

--- app/models/lynks_service_desk/application_record.rb
+++ app/models/lynks_service_desk/application_record.rb
@@ -3,11 +3,9 @@ module LynksServiceDesk
 
     self.abstract_class = true
 
-    FORMATTER_CLASS = LynksServiceDesk::Formatters::Models::Ticket
-
     [:hash_format, :json_format].each do |format_type|
       define_method format_type do
-        FORMATTER_CLASS.send(format_type, self)
+        LynksServiceDesk::Formatters::Model.send(format_type, self)
       end
     end
 
--- lib/lynks_service_desk/formatters/model.rb
+++ lib/lynks_service_desk/formatters/model.rb
@@ -2,6 +2,16 @@ module LynksServiceDesk
   module Formatters
     module Model
 
+      [:hash_format, :json_format].each do |method_name|
+        define_singleton_method method_name do |object|
+          get_object_formatter(object).send(method_name, object)
+        end
+      end
+
+      def self.get_object_formatter(object)
+        return module_eval("LynksServiceDesk::Formatters::Models::#{object.class.to_s.split("::")[1]}")
+      end
+
     end
   end
 end
