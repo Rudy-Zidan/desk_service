4e26f26c4b8f45937cc79c5173e18eac994a556b
Abdulla Mahmoud <aboodchei@gmail.com>
2017-12-24 11:17:11 +0200

    enable caching

--- app/controllers/lynks_service_desk/tickets_controller.rb
+++ app/controllers/lynks_service_desk/tickets_controller.rb
@@ -13,13 +13,19 @@ module LynksServiceDesk
     end
 
     def new
+      @a_t_c = Rails.cache.fetch("all_ticket_categories", expires_in: 48.hours) do
+        LynksServiceDesk::Category.all.map(&:hash_format)
+      end
+
       respond_to do |format|
-        format.json { render json: LynksServiceDesk::Category.all.map(&:json_format) }
+        format.json { render json: @a_t_c }
       end
     end
 
     def show
-      
+      respond_to do |format|
+        format.json { render json: LynksServiceDesk::Category.all.map(&:hash) }
+      end
     end
 
     def update
