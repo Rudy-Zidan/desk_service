627f35a87b93aea804389834114fd9ac68b8f95c
Abdulla Mahmoud <aboodchei@gmail.com>
2018-01-04 00:13:04 +0200

    re-order

--- lib/lynks_service_desk/formatters/models/ticket.rb
+++ lib/lynks_service_desk/formatters/models/ticket.rb
@@ -9,15 +9,15 @@ module LynksServiceDesk
         def self.hash_format(ticket)
           {
             id: ticket.id,
-            priority: ticket.sub_category.priority.hash_format,
             creator_id: ticket.creator_id,
             assignee_id: ticket.assignee_id,
             state: ticket.state,
             body: ticket.body,
             objects: ticket.objects.map{|object| object.hash_format},
+            priority: ticket.sub_category.priority.hash_format,
+            sub_category: ticket.sub_category.hash_format,
             created_at: ticket.created_at,
             updated_at: ticket.updated_at,
-            sub_category: ticket.sub_category.hash_format,
             # category: ticket.sub_category.category.hash_format,
           }
         end
