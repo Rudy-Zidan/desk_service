b8e041e5e09ba29aaa25c6ef1167d22958f8d8ef
Abdulla Mahmoud <aboodchei@gmail.com>
2018-01-03 20:49:35 +0200

    spacing

--- app/controllers/lynks_service_desk/tickets_controller.rb
+++ app/controllers/lynks_service_desk/tickets_controller.rb
@@ -56,7 +56,7 @@ module LynksServiceDesk
     def create
       sub_category = find_sub_category(params)
       sub_category_params = format_sub_category_options(params, sub_category)
-
+      
     rescue ActionController::ParameterMissing => e
       respond_to do |format|
         format.json { render json: {message: e.message}.to_json, status: 403 }
