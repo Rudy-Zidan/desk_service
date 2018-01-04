2d5b5104ac884d97dc55679fdde40fa6c8d31817
Abdulla Mahmoud <aboodchei@gmail.com>
2018-01-04 04:57:28 +0200

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 LynksServiceDesk::Engine.routes.draw do
-  resources :tickets, only: [:new, :index, :create, :show, :update], 
-										  defaults: {format: 'json'}
+	resources :tickets, only: [:new, :index, :create, :show, :update], 
+				defaults: {format: 'json'} do
+		member do
+			post 'transition_state'
+			post 'metrics'
+			post 'objects'
+		end
+
+		collection do
+			get 'search'
+		end
+	end
 end
