f2853d73b955f81e4947b73ab4991d2d2ef81337
Abdulla Mahmoud <aboodchei@gmail.com>
2018-01-12 01:04:53 +0200

    deprecate unused functions

--- lib/lynks_service_desk/formatters/controller.rb
+++ lib/lynks_service_desk/formatters/controller.rb
@@ -9,30 +9,32 @@ module LynksServiceDesk
         return params.permit(permitted_params)
       end
 
-      def self.query_tickets(params)
-        tickets = LynksServiceDesk::Ticket
-        joint_tables = []
-        ["ticket", "sub_category", "category", "metric"].each do |model_name|
-          query_options[model_name].each do |attribute|
-            to_look_for = params["#{model_name}_#{attribute}"].to_a + params["#{model_name}_#{attribute}s"].to_a
-            next if to_look_for.blank?
-            @tickets.send("where", "lynk")
-          end
-        end
+      # deprecated. will reconsider later
+
+      # def self.query_tickets(params)
+      #   tickets = LynksServiceDesk::Ticket
+      #   joint_tables = []
+      #   ["ticket", "sub_category", "category", "metric"].each do |model_name|
+      #     query_options[model_name].each do |attribute|
+      #       to_look_for = params["#{model_name}_#{attribute}"].to_a + params["#{model_name}_#{attribute}s"].to_a
+      #       next if to_look_for.blank?
+      #       @tickets.send("where", "lynk")
+      #     end
+      #   end
 
 
-      end
+      # end
 
-      def self.query_options
-        {
-          "ticket" => ["id", "assignee_id", "creator_id"],
-          "sub_category" => ["id", "name", "slug"],
-          "category" => ["id", "name", "slug"],
-          "metric" => ["user_id", "action"],
-          "no_tables" => ["metrics", "ticket_relation_objects"],
-          "no_fields" => ["creator_id" "assignee_id"],
-        }
-      end
+      # def self.query_options
+      #   {
+      #     "ticket" => ["id", "assignee_id", "creator_id"],
+      #     "sub_category" => ["id", "name", "slug"],
+      #     "category" => ["id", "name", "slug"],
+      #     "metric" => ["user_id", "action"],
+      #     "no_tables" => ["metrics", "ticket_relation_objects"],
+      #     "no_fields" => ["creator_id" "assignee_id"],
+      #   }
+      # end
 
     end
   end
