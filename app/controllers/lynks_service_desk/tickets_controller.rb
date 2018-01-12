9ab02a37b01273bbff8020d98c3b7c0b05149b30
Abdulla Mahmoud <aboodchei@gmail.com>
2018-01-12 08:16:31 +0200

    add query params to scopes

--- app/controllers/lynks_service_desk/tickets_controller.rb
+++ app/controllers/lynks_service_desk/tickets_controller.rb
@@ -17,37 +17,13 @@ module LynksServiceDesk
       scope = params[:scope]
 
       if scope.present? && LynksServiceDesk::Ticket.respond_to?(scope)
-        tickets = tickets.send(scope)
-      end
-
-      if params[:assignee_id].present?
-        tickets = tickets.where(assignee_id: params[:assignee_id])
-      end
-
-      if params[:creator_id].present?
-        tickets = tickets.where(creator_id: params[:creator_id])
-      end
-
-      if params[:sub_category_slug].present?
-        tickets = tickets.joins(:sub_category)
-                  .where("lynks_service_desk_sub_categories.slug = ?",
-                                      params[:sub_category_slug] )
-      end
-
-      if params[:category_slug].present?
-        tickets = tickets.joins(:category)
-                  .where("lynks_service_desk_categories.slug = ?",
-                                      params[:category_slug] )
-      end
-
-      if params[:priority_slug].present?
-        tickets = tickets.joins(:priority)
-                  .where("lynks_service_desk_priorities.slug = ?",
-                                      params[:priority_slug] )
+        tickets = query_tickets(tickets.send(scope))
+      else
+        query_tickets
       end
 
       return_hash = {}
-      return_hash[:scopes] = CONFIG.formatted_custom_scopes.keys
+      return_hash[:scopes] = CONFIG.formatted_custom_scopes.keys + [:all]
       if CONFIG.unopened_using_metrics? && !self.respond_to?(:unopened)
         return_hash[:scopes] += [:unopened]
       end
@@ -56,7 +32,9 @@ module LynksServiceDesk
 
       return_hash[:scopes].flatten!
       return_hash[:scopes].uniq!
-      return_hash[:scopes].each{|scope| return_hash[scope] = LynksServiceDesk::Ticket.send(scope).count}
+      return_hash[:scopes].each do |scope|
+        return_hash[scope] = query_tickets(LynksServiceDesk::Ticket.send(scope)).count
+      end
 
       return_hash[:tickets] = tickets.page(page).per(limit).map(&:hash_format)
 
@@ -196,6 +174,36 @@ module LynksServiceDesk
           status: 403
         }
       end
+    end
+
+
+    def query_tickets(tickets=LynksServiceDesk::Ticket)
+      if params[:assignee_id].present?
+        tickets = tickets.where(assignee_id: params[:assignee_id])
+      end
+
+      if params[:creator_id].present?
+        tickets = tickets.where(creator_id: params[:creator_id])
+      end
+
+      if params[:sub_category_slug].present?
+        tickets = tickets.joins(:sub_category)
+                  .where("lynks_service_desk_sub_categories.slug = ?",
+                                      params[:sub_category_slug] )
+      end
+
+      if params[:category_slug].present?
+        tickets = tickets.joins(:category)
+                  .where("lynks_service_desk_categories.slug = ?",
+                                      params[:category_slug] )
+      end
+
+      if params[:priority_slug].present?
+        tickets = tickets.joins(:priority)
+                  .where("lynks_service_desk_priorities.slug = ?",
+                                      params[:priority_slug] )
+      end
+      return tickets
     end
 
     def set_ticket
